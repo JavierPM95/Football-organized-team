dd8e7e2a40e66471a1472c7c651a633a4387fe30
JavierPM1995 <jaavierpm1995@gmail.com>
2020-10-22 17:41:07 -0300

    Updates

---
+++ precache-manifest.629684c2b7836353e84d56ea01ae94f7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "587986bec93ad1dc10e6e8c6c1eda9c8",
+    "url": "/Football-organized-team/index.html"
+  },
+  {
+    "revision": "fc535b8a7e9bfbaa65ee",
+    "url": "/Football-organized-team/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "b2971642e4120ff18ff0",
+    "url": "/Football-organized-team/static/css/main.65c6e253.chunk.css"
+  },
+  {
+    "revision": "fc535b8a7e9bfbaa65ee",
+    "url": "/Football-organized-team/static/js/2.d03d85aa.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Football-organized-team/static/js/2.d03d85aa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b2971642e4120ff18ff0",
+    "url": "/Football-organized-team/static/js/main.f70109bc.chunk.js"
+  },
+  {
+    "revision": "8d4d7806369a4336001f",
+    "url": "/Football-organized-team/static/js/runtime-main.79ebf383.js"
+  },
+  {
+    "revision": "aa6d54bfa22c49cf58b85efd6f82d010",
+    "url": "/Football-organized-team/static/media/soccer-field.aa6d54bf.svg"
+  }
+]);
